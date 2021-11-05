747f4113a4e98f35a4ede614532eb502d003c4c4
Bruno-M44 <bruno.maguin@posteo.net>
2021-11-05 12:24:13 +0100

    correctifs combinaisons et coûts

--- bruteforce.py
+++ bruteforce.py
@@ -10,28 +10,28 @@ class Actions:
 
     def best_result(self):
         start = time.time()
-        df = pd.read_csv(self.input_file, sep=";")
-        df.columns = ["action", "cost", "profit"]
+        df = pd.read_csv(self.input_file, sep=",")
         df["payout"] = ""
-        df["profit"] = df["profit"].str.rstrip("%").astype(float) / 100
-        df["payout"] = df["profit"] * df["cost"]
+        df["payout"] = df["profit"] / 100 * df["price"]
         max_combination = 2**len(df)
         number_combinations = 1
         best_payout = 0
+        sum_price_final = 0
         best_combination = []
         for size_combination in range(len(df)):
             for combination in combinations(df.values.tolist(),
                                             size_combination):
                 combination = list(combination)
-                sum_costs = 0
+                sum_price = 0
                 sum_payout = 0
                 for iCombination in combination:
-                    sum_costs += iCombination[1]
+                    sum_price += iCombination[1]
                     sum_payout += iCombination[3]
-                if sum_costs <= 500:
+                if sum_price <= 500:
                     if sum_payout >= best_payout:
                         best_payout = sum_payout
                         best_combination = combination
+                        sum_price_final = sum_price
                 number_combinations += 1
                 print(number_combinations, "/", max_combination)
         end = time.time()
@@ -39,6 +39,7 @@ class Actions:
         print("La meilleure combinaison est la suivante : ")
         print([action[0] for action in best_combination])
         print("pour un gain de ", best_payout, " euros")
+        print("et un coût total de :", sum_price_final, " euros")
         print("Temps de traitement : ", datetime.timedelta(seconds=elapsed))
 
 
--- optimized.py
+++ optimized.py
@@ -9,44 +9,64 @@ class Actions:
 
     def best_result(self):
         start = time.time()
-        df = pd.read_csv(self.input_file, sep=";")
-        df.columns = ["action", "cost", "profit"]
-        df = df.sort_values(by="cost", ascending=False).reset_index(
-            drop=True)
-        df = pd.DataFrame([("Aucune action", 0, "0%")],
-                          columns=df.columns).append(df, ignore_index=True)
+        df = pd.read_csv(self.input_file, sep=",")
+        df = df[df["price"] > 0]
         df["payout"] = ""
-        df["profit"] = df["profit"].str.rstrip("%").astype(float) / 100
-        df["payout"] = df["profit"] * df["cost"]
+        df["payout"] = df["profit"] / 100 * df["price"]
+        df = df.sort_values(by="price", ascending=False).reset_index(
+            drop=True)
+        df = pd.DataFrame([("Aucune action", 0, 0, 0)],
+                          columns=df.columns).append(df, ignore_index=True)
         pd.DataFrame(["Aucune action", 0, 0])
         for costs_max in range(500 + 1):
             df[costs_max] = 0
 
         for iAction in range(1, len(df)):
             for costs_max in range(1, 500 + 1):
-                if df.loc[iAction, "cost"] <= costs_max:
+                if df.loc[iAction, "price"] <= costs_max:
                     df.loc[iAction, costs_max] = max(
                         df.loc[iAction - 1, costs_max],
                         df.loc[iAction, "payout"] +
                         df.loc[
-                            iAction - 1, costs_max - df.loc[iAction, "cost"]])
+                            iAction - 1, costs_max -
+                            round(df.loc[iAction, "price"])])
                 else:
                     df.loc[iAction, costs_max] = df.loc[iAction - 1, costs_max]
 
         best_combination = []
-        for iAction in range(len(df) - 1, 1, -1):
-            if df.loc[iAction, 500] != df.loc[iAction - 1, 500]:
-                best_combination.append((df.loc[iAction, "action"]))
+        sum_price = 0
+        iAction = len(df) - 1
+        costs_max = 500
+        while iAction != 0 and costs_max != 0:
+            if df.loc[iAction, costs_max] == df.loc[iAction, costs_max - 1]:
+                costs_max -= 1
+            elif df.loc[iAction, "payout"] + \
+                    df.loc[iAction - 1, costs_max - round(df.loc[iAction,
+                                                                 "price"])] \
+                    >= df.loc[iAction - 1, costs_max]:
+                best_combination.append((df.loc[iAction, "name"]))
+                sum_price += df.loc[iAction, "price"]
+                costs_max -= round(df.loc[iAction, "price"])
+                iAction -= 1
+            else:
+                iAction -= 1
 
         end = time.time()
         elapsed = end - start
         print("La meilleure combinaison est la suivante : ")
         print(best_combination)
         print("pour un gain de ", df.iloc[-1][500], " euros")
+        print("et un coût total de :", sum_price, " euros")
         print("Temps de traitement : ", datetime.timedelta(seconds=elapsed))
         # df.to_csv("df.csv", mode='w+')
 
 
-file = "actions.csv"
-instance_action = Actions(file)
+dataset_part1 = "actions.csv"
+instance_action = Actions(dataset_part1)
 instance_action.best_result()
+
+'''
+dataset1 = "dataset1_Python+P7.csv"
+instance_action = Actions(dataset1)
+instance_action.best_result()
+'''
